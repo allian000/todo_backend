5e6f52fb0c4ff9d2aef03ac81ca3547f7f2ba5ee
allian000 <asdtacps0526@gmail.com>
2024-07-08 16:02:31 +0800

    change database url

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-database_url = "mysql+pymysql://root:123456789@localhost:3306/todo_app"
+database_url = "mysql+pymysql://root:tcust1234@localhost:3306/todo_app"
 
 engine = create_engine(
     database_url
